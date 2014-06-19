72b40b5bad10963291b91d96233a298cdc636381
Lee Prosser <lee.prosser@embersoft.co.uk>
2014-06-19 18:37:22 +0100

    Added module wrapping to stop global pollution.

--- src/bind.js
+++ src/bind.js
@@ -1,98 +1,102 @@
-function ConstructorDescription(name, targetConstructor, args)
-{
-	this.name = name;
-	this.targetConstructor = targetConstructor;
-	this.args = args;
-}
+var BindJS = (function(){
+    
+	function ConstructorDescription(name, targetConstructor, args)
+	{
+		this.name = name;
+		this.targetConstructor = targetConstructor;
+		this.args = args;
+	}
 
-function BindingRule(constructorDescriptor)
-{
-	this.descriptor = constructorDescriptor;
-	this.args = [];
-}
+	function BindingRule(constructorDescriptor)
+	{
+		this.descriptor = constructorDescriptor;
+		this.args = [];
+	}
 
-function BoundArgument(value, isDependency)
-{
-	this.value = value;
-	this.isDependency = isDependency;
-}
+	function BoundArgument(value, isDependency)
+	{
+		this.value = value;
+		this.isDependency = isDependency;
+	}
 
-function BindJS()
-{	
-	var self = this;
-	var bindings = [];
+	return function()
+	{	
+		var self = this;
+		var bindings = [];
 	
-	function BindingContext(bindingRule) {
-	
-		this.withDependency = function(name, targetConstructor) {
-			if(bindingRule.args[name])
-			{ throw "Dependency [" + bindingRule.descriptor.name + "] already has bound argument [" + name + "]"; }
+		function BindingContext(bindingRule) {
+		
+			this.withDependency = function(name, targetConstructor) {
+				if(bindingRule.args[name])
+				{ throw "Dependency [" + bindingRule.descriptor.name + "] already has bound argument [" + name + "]"; }
 
-			bindingRule.args[name] = new BoundArgument(targetConstructor, true);
-			return this;
+				bindingRule.args[name] = new BoundArgument(targetConstructor, true);
+				return this;
+			};
+			
+			this.withArgument = function(name, value) {
+				if(bindingRule.args[name])
+				{ throw "Dependency [" + bindingRule.descriptor.name + "] already has bound argument [" + name + "]"; }
+				bindingRule.args[name] = new BoundArgument(value, false);
+				return this;
+			};
 		};
 		
-		this.withArgument = function(name, value) {
-			if(bindingRule.args[name])
-			{ throw "Dependency [" + bindingRule.descriptor.name + "] already has bound argument [" + name + "]"; }
-			bindingRule.args[name] = new BoundArgument(value, false);
-			return this;
+		var getObjectDescriptor = function(targetConstructor) {
+			var functionMatcher = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+			var constructorMatcher = /function (\w*)/;
+			var text = targetConstructor.toString();
+			var matches = text.match(functionMatcher);
+			var descriptor = new ConstructorDescription();
+			var constructorName = matches[0].match(constructorMatcher)[1];
+			var constructorArgs = matches[1].split(',');
+			var descriptor = new ConstructorDescription(constructorName, targetConstructor, constructorArgs);
+			return descriptor;		
 		};
-	};
-	
-	var getObjectDescriptor = function(targetConstructor) {
-		var functionMatcher = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
-        var constructorMatcher = /function (\w*)/;
-        var text = targetConstructor.toString();
-		var matches = text.match(functionMatcher);
-		var descriptor = new ConstructorDescription();
-		var constructorName = matches[0].match(constructorMatcher)[1];
-        var constructorArgs = matches[1].split(',');
-		var descriptor = new ConstructorDescription(constructorName, targetConstructor, constructorArgs);
-		return descriptor;		
-	};
-	
-	var getOrderedArgs = function(descriptor) {
-		var bindingRule = bindings[descriptor.name];
-		var orderedArgs = [];
 		
-		for(var argName in bindingRule.args) {
-			var boundArg = bindingRule.args[argName];
-			var valueToUse;
-			if(boundArg.isDependency) 
-			{ 
-				var dependency = self.get(boundArg.value);
-				orderedArgs.push(dependency); 
+		var getOrderedArgs = function(descriptor) {
+			var bindingRule = bindings[descriptor.name];
+			var orderedArgs = [];
+			
+			for(var argName in bindingRule.args) {
+				var boundArg = bindingRule.args[argName];
+				var valueToUse;
+				if(boundArg.isDependency) 
+				{ 
+					var dependency = self.get(boundArg.value);
+					orderedArgs.push(dependency); 
+				}
+				else 
+				{ orderedArgs.push(boundArg.value); }
 			}
-			else 
-			{ orderedArgs.push(boundArg.value); }
-		}
+			
+			return orderedArgs;
+		};
 		
-		return orderedArgs;
-	};
-	
-	var createInstance = function(targetConstructor, args) {
-		var finalArgs = [null].concat(args);
-		var factoryFunction = targetConstructor.bind.apply(targetConstructor, finalArgs);
-		return new factoryFunction();
-	};
-	
-	this.bind = function(targetConstructor) {
-		var descriptor = getObjectDescriptor(targetConstructor);
-		if(bindings[descriptor.name]) 
-		{ throw "Dependency [" + descriptor.name + "] is already bound"; }
+		var createInstance = function(targetConstructor, args) {
+			var finalArgs = [null].concat(args);
+			var factoryFunction = targetConstructor.bind.apply(targetConstructor, finalArgs);
+			return new factoryFunction();
+		};
 		
-		var bindingRule = new BindingRule(descriptor);
-		bindings[descriptor.name] = bindingRule;
-		return new BindingContext(bindingRule);
-	};
-	
-	this.get = function(targetConstructor) {
-		var descriptor = getObjectDescriptor(targetConstructor);
-		if(!bindings[descriptor.name])
-		{ throw "There is no available binding for [" + descriptor.name + "] confirm you have bound it"; }
+		this.bind = function(targetConstructor) {
+			var descriptor = getObjectDescriptor(targetConstructor);
+			if(bindings[descriptor.name]) 
+			{ throw "Dependency [" + descriptor.name + "] is already bound"; }
+			
+			var bindingRule = new BindingRule(descriptor);
+			bindings[descriptor.name] = bindingRule;
+			return new BindingContext(bindingRule);
+		};
 		
-		var orderedArgs = getOrderedArgs(descriptor);
-		return createInstance(targetConstructor, orderedArgs);
-	};
-}
+		this.get = function(targetConstructor) {
+			var descriptor = getObjectDescriptor(targetConstructor);
+			if(!bindings[descriptor.name])
+			{ throw "There is no available binding for [" + descriptor.name + "] confirm you have bound it"; }
+			
+			var orderedArgs = getOrderedArgs(descriptor);
+			return createInstance(targetConstructor, orderedArgs);
+		};
+	}
+	
+})();
